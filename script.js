8cd6f28f26b9c79e3fcb7c0f62d675d1e82b054b
CodeYourFuture <nawal.careem@yahoo.com>
2020-06-10 00:49:21 +0100

    removed exta divs

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-// popup modal
-$('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').trigger('focus')
-  })
+// var numCards = 8;
+
+// var pickedColor;
+
+// var resetButton = document.querySelector("#reset");
+// var modeButtons = document.querySelectorAll(".mode");
+
+let cardsColor = document.body.querySelectorAll('.card')
+let colors = [  "rgb(138, 6, 6)", //red
+                "rgb(246, 255, 0)",   //yellow
+                "rgb(26, 65, 97)",   //blue
+                "rgb(43, 97, 26)",    // green
+                "rgb122, 27, 57)",   //plum 
+                "rgb(129, 230, 28)" //light green
+];
+for (i=0; i<cardsColor.length; i++) {cardsColor.style.backgroundColor = colors[i]}
+ 
+
+
+
+    
+
+// for(i=0; i<= card.length; i++) {  
+//    card[i].addeventListeners('click', function() {   
+//          alert('option was clicked'); 
+//          });}
+
